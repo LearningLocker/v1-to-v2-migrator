8071cd015aa4dbba109223f8ea02380c465d9b32
Ryan Smith <0ryansmith1994@gmail.com>
2017-02-09 10:39:57 +0000

    Fixes connection timeouts.

--- src/migrateLocalData.js
+++ src/migrateLocalData.js
@@ -3,82 +3,97 @@ const mongodb = require('mongodb');
 const config = require('./config');
 const logStep = require('./logStep');
 
-const migrateOrgs = (db) => {
-  const orgUpdate = { $set: { organisation: mongodb.ObjectId(config.target.orgId) } };
-  const opts = { multi: true };
-  return Promise.all([
-    db.collection('lrs').update({}, orgUpdate, opts),
-    db.collection('client').update({}, orgUpdate, opts),
-    db.collection('statements').update({}, orgUpdate, opts),
-  ]);
-};
+const mongoUrl = `mongodb://localhost:27017/${config.local.database}`;
+const connect = (log, fn) =>
+  mongodb.MongoClient.connect(mongoUrl, {
+    socketOptions: {
+      connectTimeoutMS: Number(config.local.connectTimeoutMS || 30000),
+      socketTimeoutMS: Number(config.local.socketTimeoutMS || 30000),
+    },
+  }).then((db) => {
+    console.log(`Started ${log}`);
+    const result = fn(db);
+    result.then(() => {
+      db.close();
+      console.log(`Finished ${log}`);
+    });
+    return result;
+  });
 
-const migrateStatementsClient = (db, client) =>
-  db.collection('statements').update(
-    {
-      client_id: client._id.toString(),
-      client: {$exists: false}
-    },
-    {
-      $set: { client: client._id }
-    },
-    { multi: true }
+const updateOrg = collection =>
+  connect(`updating orgs for ${collection}`, db =>
+    db.collection(collection).update(
+      {},
+      { $set: { organisation: mongodb.ObjectId(config.target.orgId) } },
+      { multi: true }
+    )
   );
 
-const migrateScopes = (db, client) => {
+const migrateOrgs = () =>
+  Promise.all([
+    updateOrg('lrs'),
+    updateOrg('client'),
+    updateOrg('statements'),
+  ]);
+
+const migrateStatementsClient = client =>
+  connect(`migrating statements client for ${client._id}`, db =>
+    db.collection('statements').update(
+      { client_id: client._id.toString() },
+      { $set: { client: client._id } },
+      { multi: true }
+    )
+  );
+
+const updateClient = (client, update) =>
+  connect(`updating client ${client._id}`, db =>
+    db.collection('client').update({ _id: client._id }, update)
+  );
+
+const migrateScopes = (client) => {
   if (client.scopes.indexOf('all/read') !== -1) {
-    return db.collection('client').update(
-      { _id: client._id },
-      { $push: { scopes: 'xapi/read' } }
-    ).then(() =>
-      db.collection('client').update(
-        { _id: client._id },
-        { $pull: { scopes: 'all/read' } }
-      )
+    return updateClient(client, { $push: { scopes: 'xapi/read' } }).then(() =>
+      updateClient(client, { $pull: { scopes: 'all/read' } })
     );
   }
   return Promise.resolve();
 };
 
-const migrateAuthority = (db, client) =>
-  db.collection('client').update({ _id: client._id }, {
+const migrateAuthority = client =>
+  updateClient(client, {
     $set: {
       title: `V1 Client - ${client.created_at}`,
-      authority: JSON.stringify(client.authority)
+      authority: (
+        client.authority.constructor === Object ?
+        JSON.stringify(client.authority) :
+        client.authority
+      ),
     }
   });
 
+const getClients = () =>
+  connect('getting clients', db =>
+    db.collection('client').find().toArray().then((clients) => {
+      db.close();
+      return clients;
+    })
+  );
+
 module.exports = () => {
   logStep('Migrating local data');
-  const mongoUrl = `mongodb://localhost:27017/${config.local.database}`;
-  return new Promise((resolve, reject) => {
-    mongodb.MongoClient.connect(mongoUrl, (err, db) => {
-      if (err) return reject(err);
+  return getClients().then((clients) => {
+    const orgMigrations = migrateOrgs();
+    const clientMigrations = clients.map(client =>
+      Promise.all([
+        migrateStatementsClient(client),
+        migrateScopes(client),
+        migrateAuthority(client),
+      ])
+    );
 
-      const orgMigrations = migrateOrgs(db);
-      const clientMigrations = Promise.all(
-        db.collection('client').find().toArray().then(clients =>
-          clients.map(client => {
-            const clientId = client._id.toString();
-            const clientIdFilter = { _id: client._id };
-
-            console.log(`Migrating ${clientId} client`);
-            return Promise.all([
-              migrateStatementsClient(db, client),
-              migrateScopes(db, client),
-              migrateAuthority(db, client),
-            ]).then(() => {
-              console.log(`Migrated ${clientId} client`);
-              db.close();
-            });
-          })
-        )
-      );
-
-      return resolve(Promise.all([
-        orgMigrations,
-        clientMigrations,
-      ]));
-    });
+    return Promise.all([
+      orgMigrations,
+      clientMigrations,
+    ]);
   });
 };
