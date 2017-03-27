3f3c5c5b8db5bc72b4a263b65fb4814a8add3cf3
James Mullaney <james@ht2.co.uk>
2017-03-27 10:44:21 +0100

    Update restoreTargetData.js

--- src/restoreTargetData.js
+++ src/restoreTargetData.js
@@ -12,6 +12,6 @@ module.exports = () => {
   const password = createCLIOption('-p', targetDb.password);
   const authDb = createCLIOption('--authenticationDatabase', targetDb.authenticationDatabase);
   const ssl = createCLIFlag('--ssl', targetDb.ssl);
-  const quiet = createCLI('--quiet', targetDb.quiet);
+  const quiet = createCLIFlag('--quiet', targetDb.quiet);
   return exec(`mongorestore ${ssl} --db=${targetDb.name} --host ${targetDb.hosts} ${authDb} ${user} ${password} ${quiet} --noIndexRestore --gzip ${config.local.targetDumpLocation}/${config.local.database}`);
 };
