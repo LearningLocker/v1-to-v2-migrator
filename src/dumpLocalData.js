3333f2ab73908bc182634a849126ac5e7676b56b
David Pesce <davidpesce@gmail.com>
2018-02-14 14:38:43 -0500

    a few tweaks to make it more in line with other CLI flags

--- src/dumpLocalData.js
+++ src/dumpLocalData.js
@@ -1,8 +1,12 @@
 const exec = require('./exec');
 const config = require('./config');
 const logStep = require('./logStep');
+const createCLIFlag = require('./createCLIFlag');
+
+const localDb = config.local.database;
+const compression = createCLIFlag('--gzip', localDb.compression);
 
 module.exports = () => {
   logStep('Dumping local data', true);
-  return exec(`mongodump --db ${config.local.database} ${config.local.compression} --out ${config.local.targetDumpLocation}`);
+  return exec(`mongodump --db ${localDb.database} ${compression} --out ${localDb.targetDumpLocation}`);
 };
